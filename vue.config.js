595054f0e063c353e71d8382cef060e84b1c5d6a
Julian Lau <julian.lau@aol.com>
2021-11-21 00:05:52 +0100

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    pwa: {
-      name: 'Fee Calculator For Paypal Fees',
-      themeColor: '#000809',
-      msTileColor: '#000809',
-      backgroundColor: '#000809',
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'default',
-      workboxOptions: {
-        exclude: [/\.map$/, /_redirects/],
-      }
+  pwa: {
+    name: 'Libria',
+    themeColor: '#000809',
+    msTileColor: '#000809',
+    backgroundColor: '#000809',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    workboxOptions: {
+      exclude: [/\.map$/, /_redirects/],
     }
-  }
+  },
+}
   
